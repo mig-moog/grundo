7672d8828e78e661eecca2c4865ce88e0f536230
mig-moog <orangej0312@gmail.com>
2021-01-13 21:30:32 -0500

    grundo beginnings

--- src/main.js
+++ src/main.js
@@ -11,5 +11,16 @@ class P extends Phaser.Scene {
 }
 
 const game = new Phaser.Game({
-    scene: [P, MainScene]
+    scene: [P, MainScene],
+    scale: {
+        width: 128,
+        height: 128,
+        zoom: Phaser.Scale.ZOOM_4X
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true
+        }
+    }
 });
--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -1,6 +1,12 @@
 export default class MainScene extends Phaser.Scene {
+    /**@type {Phaser.GameObjects.Sprite} the incredible */
+    grundo;
+    /**@type {object} */
+    keys;
+    
     create() {
-
+        this.grundo = this.add.rectangle(this.scale.width/2, this.scale.height/2, 32, 32);
+        this.physics.add.existing(this.grundo);
     }
 
     update() {
