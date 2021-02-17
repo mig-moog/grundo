0f1eace330fd351d8c7481854a1db254e6daf496
mig-moog <orangej0312@gmail.com>
2021-02-17 08:53:46 -0500

    Teeth pairs now move together
    Other teeth also won't move if teeth are already moving

--- src/objects/Teeth.js
+++ src/objects/Teeth.js
@@ -1,6 +1,7 @@
 export class Teeth extends Phaser.Physics.Arcade.Group {
     pairs = [];
-    
+    moving = false;
+
     /**@param {Phaser.Scene} scn */
     constructor(scn) {
         super(scn.physics.world, scn);
@@ -23,6 +24,11 @@ export class Teeth extends Phaser.Physics.Arcade.Group {
         this.children.iterate((e, ix) => {
             e.parent = this;
             e.ix = this;
+
+            if (e.name === 'top')
+                e.btm = this.children.entries[ix + 1];
+            else
+                e.top = this.children.entries[ix - 1];
 
             e.body
                 .setImmovable(true)
--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -18,35 +18,49 @@ export class MainScene extends Phaser.Scene {
 
         this.ob = new Ob(this, center.x, center.y, this.input.keyboard.createCursorKeys());
         this.teeth = new Teeth(this);
+
         this.stopZone = this.add.zone(0, center.y, this.scale.width, 1).setOrigin(0, 0);
         this.physics.add.existing(this.stopZone, true);
+
         this.teethLimits = this.physics.add.staticGroup([
             // 24 bc it's half of a tooth's width plus 1 more for space from teethLimits
             this.add.zone(0, -24, this.scale.width, 1).setOrigin(0, 0),
             this.add.zone(0, this.scale.height + 24, this.scale.width, 1).setOrigin(0, 0)
         ]);
 
+        const toothSpeed = 50;
+
         this.physics.add.collider(this.ob, this.teeth, (ob, tooth) => {
-            if (tooth.name === 'btm') {
-                tooth.setVelocityY(-50);
-            } else {
-                tooth.setVelocityY(50);
+            if (!this.teeth.moving) {
+                if (tooth.name === 'btm' && tooth.body.touching.up) {
+                    tooth.top.setVelocityY(toothSpeed);
+                    tooth.setVelocityY(-toothSpeed);
+
+                    this.teeth.moving = true;
+                } else if (tooth.name === 'top' && tooth.body.touching.down) {
+                    tooth.setVelocityY(toothSpeed);
+                    tooth.btm.setVelocityY(-toothSpeed);
+
+                    this.teeth.moving = true;
+                }
             }
         });
         this.physics.add.collider(this.stopZone, this.teeth, (stopZone, tooth) => {
             tooth.body.stop();
             if (tooth.name === 'btm') {
-                tooth.setVelocityY(50);
+                tooth.setVelocityY(toothSpeed);
             } else {
-                tooth.setVelocityY(-50);
+                tooth.setVelocityY(-toothSpeed);
             }
         });
         this.physics.add.collider(this.teethLimits, this.teeth, (limit, tooth) => {
             tooth.body.stop();
             if (tooth.name === 'btm') {
                 tooth.setY(180);
+                this.teeth.moving = false;
             } else {
                 tooth.setY(0);
+                this.teeth.moving = false;
             }
         });
     }
